050063164483ec9d25ca1c4e6c4edc98948604b8
Dmitriy <redtoss@gmail.com>
2022-12-23 16:30:08 +0300

    -add details, retrofit, mvvm

---
+++ app/src/main/java/com/example/phonesapp1212/presentation/viewmodel/CatDetailViewModel.kt
@@ -0,0 +1,45 @@
+package com.example.phonesapp1212.presentation.viewmodel
+
+import android.util.Log
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.data.model.cats.Breed
+import com.example.data.model.cats.CatBreedDetails
+import com.example.data.repository.CatBreedRepository
+import com.example.data.repository.CatBreedRepositoryImpl
+import kotlinx.coroutines.async
+import kotlinx.coroutines.launch
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
+class CatDetailViewModel : ViewModel() {
+
+    //insert di next
+    private val catBreedRepository: CatBreedRepository = CatBreedRepositoryImpl()
+    private val mMutableLiveData = MutableLiveData<CatBreedDetails>()
+    val liveData: LiveData<CatBreedDetails> = mMutableLiveData
+
+    fun getCatInfo(id: String) {
+        viewModelScope.launch {
+            val  response = catBreedRepository.getBreed(id)
+            response.enqueue(object : Callback<CatBreedDetails> {
+                override fun onResponse(call: Call<CatBreedDetails>, response: Response<CatBreedDetails>) {
+                    Log.d("pokemon", "onResponse: ${call.request()}")
+                    if (response.body() != null) {
+                        Log.d("pokemon", "onResponse: ${response.body()}")
+
+                        mMutableLiveData.postValue(response.body())
+                    }
+                }
+
+                override fun onFailure(call: Call<CatBreedDetails>, t: Throwable) {
+
+                    Log.d("pokemon", "onResponse: ${t.toString()}")
+                }
+            })
+        }
+    }
+}
