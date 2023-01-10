70fc71b5ee76a748a3156f42ba65f2a0b4b65b03
Dmitriy <redtoss@gmail.com>
2023-01-11 00:41:43 +0300

    -add Dao. -add RoomDatabase -add RoomRepository

--- data/src/main/java/com/example/data/room/model/CatEntity.kt
+++ data/src/main/java/com/example/data/room/model/CatEntity.kt
@@ -4,21 +4,43 @@ import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
+import com.example.phonesapp1212.domain.model.Cat
 
 @Entity(
     tableName = "cat_table",
-    indices = [Index(value = ["id"])]
+    indices = [Index(value = ["title"])]
 )
 data class CatEntity(
     @PrimaryKey(autoGenerate = true)
-    @ColumnInfo(name = "id")
     val id: Long? = null,
     @ColumnInfo(name = "title")
     val title: String,
     @ColumnInfo(name = "description")
     val description: String,
+    @ColumnInfo(name = "image link")
+    val image: String
+) {
 
-    @ColumnInfo(name="image link")
-    val image: String)
+    fun toCat(catEntity: CatEntity): Cat {
+        return Cat(
+            title = title,
+            description = description,
+            image = image
+        )
+    }
+
+    companion object {
+        fun fromCatToCatEntity(cat: Cat): CatEntity {
+
+            return CatEntity(
+                id = 0,
+                title = cat.title,
+                description = cat.description,
+                image = cat.image
+            )
+        }
+    }
+
+}
 
 
---
+++ domain/src/main/java/com/example/phonesapp1212/domain/model/Cat.kt
@@ -0,0 +1,7 @@
+package com.example.phonesapp1212.domain.model
+
+data class Cat(
+    val title: String,
+    val description: String,
+    val image: String
+)
